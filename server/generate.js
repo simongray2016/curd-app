9ba1175dda2b6a6c667b16f1af2a16cb95b33ca5
Daniel Trương <simongray2016@gmail.com>
2021-11-27 12:04:15 +0700

    mock rest api

--- server/generate.js
+++ server/generate.js
@@ -4,13 +4,13 @@ var database = { products: [] };
 
 for (var i = 1; i <= 100; i++) {
   database.products.push({
-    id: faker.datatype.uuid(),
+    id: i,
     name: faker.commerce.productName(),
     status: faker.helpers.randomize(["Active", "Suspended", "Expriring"]),
     category: faker.helpers.randomize(["Men", "Ladies", "Unisex"]),
     createAt: faker.date.past(),
     material: faker.commerce.productMaterial(),
-    img: faker.random.image(),
+    imgUrl: faker.random.image(),
     description: faker.commerce.productDescription(),
     stock: faker.datatype.number(100),
     price: faker.datatype.number(100000),
