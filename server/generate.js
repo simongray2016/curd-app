9b4d550ac88e6365caf8c38848a156330c194145
Daniel Trương <simongray2016@gmail.com>
2021-11-27 10:03:23 +0700

    mock rest api

---
+++ server/generate.js
@@ -0,0 +1,20 @@
+var faker = require("faker");
+
+var database = { products: [] };
+
+for (var i = 1; i <= 100; i++) {
+  database.products.push({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    status: faker.helpers.randomize(["Active", "Suspended", "Expriring"]),
+    category: faker.helpers.randomize(["Men", "Ladies", "Unisex"]),
+    createAt: faker.date.past(),
+    material: faker.commerce.productMaterial(),
+    img: faker.random.image(),
+    description: faker.commerce.productDescription(),
+    stock: faker.datatype.number(100),
+    price: faker.datatype.number(100000),
+  });
+}
+
+console.log(JSON.stringify(database));
