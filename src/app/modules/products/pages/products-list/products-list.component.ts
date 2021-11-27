2f5747717e74f85fa12b3d3ba9a27e024f77bf1d
Daniel Trương <simongray2016@gmail.com>
2021-11-27 15:41:59 +0700

    implement ui products list

--- src/app/modules/products/pages/products-list/products-list.component.ts
+++ src/app/modules/products/pages/products-list/products-list.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { finalize, Observable } from 'rxjs';
+import {
+  GetProductsPaginatedOption,
+  IProduct,
+} from 'src/app/models/product.model';
+import { GlobalStateService } from 'src/app/services/global-state.service';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.scss']
+  styleUrls: ['./products-list.component.scss'],
 })
 export class ProductsListComponent implements OnInit {
+  products$: Observable<IProduct[]>;
 
-  constructor() { }
+  displayedColumns = [
+    'selection',
+    'img',
+    'name',
+    'price',
+    'stock',
+    'created date',
+    'category',
+    'status',
+    'action',
+  ];
+
+  constructor(
+    private _productsService: ProductsService,
+    private _globalStateService: GlobalStateService
+  ) {
+    this.products$ = this._productsService.products$;
+    this.getProductsPaginated({ page: 1, limit: 20 });
+  }
 
   ngOnInit(): void {
   }
 
+  getProductsPaginated(option: GetProductsPaginatedOption) {
+    this._globalStateService.setLoading(true);
+
+    this._productsService
+      .getProductsPaginated(option)
+      .pipe(finalize(() => this._globalStateService.setLoading(false)))
+      .subscribe((product) => this._productsService.setProductsList(product));
+  }
+
+  announceSortChange(event: any) {
+    
+  }
 }
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -1,19 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProductsRoutingModule } from './products-routing.module';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+
 import { ProductsListComponent } from './pages/products-list/products-list.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
-
 @NgModule({
-  declarations: [
-    ProductsListComponent,
-    ProductDetailComponent
-  ],
+  declarations: [ProductsListComponent, ProductDetailComponent],
   imports: [
     CommonModule,
-    ProductsRoutingModule
-  ]
+    ProductsRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatSelectModule,
+  ],
 })
-export class ProductsModule { }
+export class ProductsModule {}
